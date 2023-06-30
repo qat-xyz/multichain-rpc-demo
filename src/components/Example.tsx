68e91367bfe163b1b0fd977406eb0fe62c9809b8
fareed <digital.flowers@hotmail.com>
2023-07-01 01:16:02 +0400

    editor

--- src/components/Example.tsx
+++ src/components/Example.tsx
@@ -1,4 +1,5 @@
 import {
+  Box,
   Button,
   ButtonGroup,
   HStack,
@@ -9,11 +10,10 @@ import {
   VStack,
 } from "@chakra-ui/react";
 import { javascript } from "@codemirror/lang-javascript";
-import { EditorView } from "@codemirror/view";
 import { atomone } from "@uiw/codemirror-theme-atomone";
 import CodeMirror from "@uiw/react-codemirror";
 import { FunctionComponent, useCallback, useRef, useState } from "react";
-import { IoIosCopy, IoIosPlay, IoMdList } from "react-icons/io";
+import { IoIosCopy, IoIosPlay, IoIosTrash, IoMdList } from "react-icons/io";
 
 import { formatCode } from "../utils/formatCode";
 
@@ -74,7 +74,7 @@ export const Example: FunctionComponent<{ example: any }> = ({ example }) => {
               data +
               `// ${method}\n${args
                 .map(arg =>
-                  formatCode(JSON.stringify(arg), {
+                  formatCode(JSON.stringify(arg, null, 2), {
                     semi: false,
                   })
                     .trim()
@@ -94,136 +94,168 @@ export const Example: FunctionComponent<{ example: any }> = ({ example }) => {
   }, [code, setLog]);
 
   return (
-    <Stack
-      alignItems={"stretch"}
-      justifyContent={"stretch"}
-      direction={["column", "column", "row"]}
-    >
-      <VStack alignItems={"stretch"} flex={3} spacing={0}>
-        <HStack
-          backgroundColor={"gray.600"}
-          userSelect={"none"}
-          height={"48px"}
-          shadow={"md"}
-          paddingY={2}
-          paddingX={4}
-        >
-          <Text color={"whiteAlpha.700"} textAlign={"left"} noOfLines={1} flex={1}>
-            Editable Example
-          </Text>
-          <ButtonGroup
-            display={{
-              base: "flex",
-              lg: "none",
-            }}
+    <Box>
+      <Stack
+        spacing={1}
+        direction={{
+          base: "column",
+          lg: "row",
+        }}
+      >
+        <VStack alignItems={"stretch"} spacing={0} flex={1}>
+          <HStack
+            backgroundColor={"gray.600"}
+            userSelect={"none"}
+            height={"48px"}
+            shadow={"md"}
+            paddingY={2}
+            paddingX={4}
           >
-            <IconButton
-              aria-label={"format"}
-              colorScheme={"messenger"}
-              size={"sm"}
-              icon={<IoMdList />}
-              onClick={() => setCode(formatCode(code))}
-            />
-            <IconButton
-              aria-label={"copy"}
-              colorScheme={"teal"}
-              size={"sm"}
-              icon={<IoIosCopy />}
-              onClick={onCopy}
-            />
-            <IconButton
-              aria-label={"run"}
-              colorScheme={"primary"}
-              size={"sm"}
-              icon={<IoIosPlay />}
-              onClick={run}
-            />
-          </ButtonGroup>
-          <ButtonGroup
-            display={{
-              base: "none",
-              lg: "flex",
-            }}
-          >
-            <Button
-              colorScheme={"messenger"}
-              size={"sm"}
-              leftIcon={<IoMdList />}
-              onClick={() => setCode(formatCode(code))}
+            <Text color={"whiteAlpha.700"} textAlign={"left"} noOfLines={1} flex={1}>
+              Editable Example
+            </Text>
+            <ButtonGroup
+              display={{
+                base: "flex",
+                lg: "none",
+              }}
             >
-              Format
-            </Button>
-            <Button colorScheme={"teal"} size={"sm"} leftIcon={<IoIosCopy />} onClick={onCopy}>
-              {hasCopied ? "Copied" : "Copy"}
-            </Button>
-            <Button colorScheme={"primary"} size={"sm"} leftIcon={<IoIosPlay />} onClick={run}>
-              Run
-            </Button>
-          </ButtonGroup>
-        </HStack>
-        <CodeMirror
-          value={code}
-          theme={atomone}
-          height={"300px"}
-          minHeight={"100%"}
-          extensions={[javascript(), EditorView.lineWrapping]}
-          onChange={value => setCode(value)}
-          style={{
-            flex: 1,
-          }}
-        />
-      </VStack>
-      <VStack alignItems={"stretch"} flex={2} backgroundColor={"#272c35"}>
-        <HStack
-          backgroundColor={"gray.600"}
-          userSelect={"none"}
-          height={"48px"}
-          shadow={"md"}
-          paddingY={2}
-          paddingX={4}
-        >
-          <Text color={"whiteAlpha.700"} textAlign={"left"} flex={1}>
-            Result
-          </Text>
-          <Button
-            colorScheme={"teal"}
-            size={"sm"}
-            leftIcon={<IoIosCopy />}
-            onClick={onCopyResult}
-            display={{
-              base: "none",
-              lg: "flex",
-            }}
-          >
-            {hasCopiedResult ? "Copied" : "Copy"}
-          </Button>
-          <IconButton
-            aria-label={"copy result"}
-            colorScheme={"teal"}
-            size={"sm"}
-            icon={<IoIosCopy />}
-            onClick={onCopyResult}
-            display={{
-              base: "flex",
-              lg: "none",
+              <IconButton
+                aria-label={"format"}
+                colorScheme={"messenger"}
+                size={"sm"}
+                icon={<IoMdList />}
+                onClick={() => setCode(formatCode(code))}
+              />
+              <IconButton
+                aria-label={"copy"}
+                colorScheme={"teal"}
+                size={"sm"}
+                icon={<IoIosCopy />}
+                onClick={onCopy}
+              />
+              <IconButton
+                aria-label={"run"}
+                colorScheme={"primary"}
+                size={"sm"}
+                icon={<IoIosPlay />}
+                onClick={run}
+              />
+            </ButtonGroup>
+            <ButtonGroup
+              display={{
+                base: "none",
+                lg: "flex",
+              }}
+            >
+              <Button
+                colorScheme={"messenger"}
+                size={"sm"}
+                leftIcon={<IoMdList />}
+                onClick={() => setCode(formatCode(code))}
+              >
+                Format
+              </Button>
+              <Button colorScheme={"teal"} size={"sm"} leftIcon={<IoIosCopy />} onClick={onCopy}>
+                {hasCopied ? "Copied" : "Copy"}
+              </Button>
+              <Button colorScheme={"primary"} size={"sm"} leftIcon={<IoIosPlay />} onClick={run}>
+                Run
+              </Button>
+            </ButtonGroup>
+          </HStack>
+          <CodeMirror
+            value={code}
+            theme={atomone}
+            height={"100%"}
+            maxHeight={"600px"}
+            extensions={[javascript()]}
+            onChange={value => setCode(value)}
+            style={{
+              flex: 1,
             }}
           />
-        </HStack>
-        <CodeMirror
-          value={log}
-          theme={atomone}
-          editable={false}
-          height={"300px"}
-          minHeight={"100%"}
-          extensions={[javascript(), EditorView.lineWrapping]}
-          basicSetup={{
-            lineNumbers: false,
+        </VStack>
+        <VStack
+          alignItems={"stretch"}
+          spacing={0}
+          width={{
+            base: "100%",
+            lg: "35%",
           }}
-          style={{
-            flex: 1,
-          }}
-        />
-      </VStack>
+        >
+          <HStack
+            backgroundColor={"gray.600"}
+            userSelect={"none"}
+            height={"48px"}
+            shadow={"md"}
+            paddingY={2}
+            paddingX={4}
+          >
+            <Text color={"whiteAlpha.700"} textAlign={"left"} flex={1}>
+              Output
+            </Text>
+            <ButtonGroup
+              display={{
+                base: "none",
+                lg: "flex",
+              }}
+            >
+              <Button
+                colorScheme={"blackAlpha"}
+                size={"sm"}
+                leftIcon={<IoIosTrash />}
+                onClick={() => setLog("")}
+              >
+                Clear
+              </Button>
+              <Button
+                colorScheme={"teal"}
+                size={"sm"}
+                leftIcon={<IoIosCopy />}
+                onClick={onCopyResult}
+              >
+                {hasCopiedResult ? "Copied" : "Copy"}
+              </Button>
+            </ButtonGroup>
+            <ButtonGroup
+              display={{
+                base: "flex",
+                lg: "none",
+              }}
+            >
+              <IconButton
+                aria-label={"clear"}
+                colorScheme={"blackAlpha"}
+                size={"sm"}
+                icon={<IoIosTrash />}
+                onClick={() => setLog("")}
+              />
+              <IconButton
+                aria-label={"copy result"}
+                colorScheme={"teal"}
+                size={"sm"}
+                icon={<IoIosCopy />}
+                onClick={onCopyResult}
+              />
+            </ButtonGroup>
+          </HStack>
+          <CodeMirror
+            value={log}
+            theme={atomone}
+            editable={false}
+            height={"100%"}
+            maxHeight={"600px"}
+            extensions={[javascript()]}
+            basicSetup={{
+              lineNumbers: false,
+            }}
+            style={{
+              flex: 1,
+            }}
+          />
+        </VStack>
+      </Stack>
       <iframe
         ref={iframeRef}
         title={"runner"}
@@ -231,6 +263,6 @@ export const Example: FunctionComponent<{ example: any }> = ({ example }) => {
         height={"100px"}
         style={{ display: "none" }}
       />
-    </Stack>
+    </Box>
   );
 };
